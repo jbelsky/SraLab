a24e4b1f70e87bd263ac03c2d2bcc7e1b03b750f
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2018-11-17 16:37:21 -0500

    Developed framework to store class nominations as dict of 2-D np arrays Also included method to load in each peer provision

--- ProvisionsReciprocalFriends.py
+++ ProvisionsReciprocalFriends.py
@@ -11,8 +11,35 @@ import numpy as np
 import os
 import glob
 import argparse
+from collections import OrderedDict
 
 import classmatrix
+
+def GetClassFriendshipMatrix(_friendship_df):
+
+	# Initialize the class_friendship_matrix
+	classFriendship_mat = np.empty(_friendship_df.shape, dtype = object)
+
+	# Iterate through each row
+	for i in range(0, classFriendship_mat.shape[0]):
+
+		for j in range(classFriendship_mat.shape[1]):
+
+			# Get the given and received status
+			isGiven = True if _friendship_df.iloc[i, j] == 1 else False
+			isReceived = True if _friendship_df.iloc[i, j] == 1 else False
+
+			if isGiven and isReceived:
+				classFriendship_mat[i, j] = "reciprocated"
+			elif isGiven:
+				classFriendship_mat[i, j] = "given"
+			elif isReceived:
+				classFriendship_mat[i, j] = "received"
+			else:
+				classFriendship_mat[i, j] = "none"
+
+	# Return the friendship matrix
+	return classFriendship_mat
 
 parser = argparse.ArgumentParser()
 parser.add_argument("friendship_nom_file", help = "Friendship Nominations File (.xlsx)")
@@ -25,38 +52,37 @@ wb = openpyxl.load_workbook(args.friendship_nom_file)
 class_sn = wb.sheetnames
 class_sn.sort()
 
+# Set up the friendship_matrix dict
+classFriendship_dict = OrderedDict()
+
 for c in class_sn:
 
+	# Import the nominations matrix
 	friendship_df, gender_s = classmatrix.GetDataMatrix(wb[c])
 
-	given = np.array(friendship_df.iloc[:,0], dtype = np.int)
-	received = np.array(friendship_df.iloc[0], dtype = np.int)
-	friendship_status = np.empty(given.size, dtype = object)
+	# Get the friendship matrix
+	classFriendship_dict[c] = GetClassFriendshipMatrix(friendship_df)
 
-	for i in range(0, friendship_status.size):
+# Load in each of the provision matrices
+peerProv_xlsx_files = glob.glob(args.peer_provisions_dir + "/*xlsx")
+peerProv_xlsx_files.sort()
 
-		# Get the given and received status
-		isGiven = True if given[i] == 1 else False
-		isReceived = True if received[i] == 1 else False
+# Iterate through each peer provision
+for pp_xls in peerProv_xlsx_files:
 
-		if isGiven and isReceived:
-			friendship_status[i] = "reciprocated"
-		elif isGiven:
-			friendship_status[i] = "given"
-		elif isReceived:
-			friendship_status[i] = "received"
-		else:
-			friendship_status[i] = "none"
+	pp_wb = openpyxl.load_workbook(pp_xls)
 
-	break
+	# Iterate through each class_sn
+	for c in class_sn:
 
-# Get the friendship status
-
+		try:
+			data_df, gender_s = classmatrix.GetDataMatrix(pp_wb[c])
+		except:
+			print("%s is not in %s, skipping..." % (c, pp_xls))
+			continue
 '''
 
 # Load in the peer provisions
-peerProv_xls_files = glob.glob("c:/usr/opt/cygwin64/home/belskyj/JAB_GitHub/SraLab/data/Orland_Park/Peer_Provisions/*.xlsx")
-peerProv_xls_files.sort()
 for pp_xls in peerProv_xls_files:
 
 	pp_wb = openpyxl.load_workbook(pp_xls)
@@ -68,7 +94,7 @@ for pp_xls in peerProv_xls_files:
 
 		print("%s\t%s" % (os.path.basename(pp_xls), sheet_name))
 
-		data_df, gender_s = classmatrix.GetDataMatrix(wb[sheet_name])
+
 
 		break
 

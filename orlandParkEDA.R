7b9232f8a6e2618bd356a4bf8abe1389d2363df7
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2018-12-19 05:54:37 -0500

    Added plotting and lm to orlandParkEDA.R

--- orlandParkEDA.R
+++ orlandParkEDA.R
@@ -9,8 +9,57 @@ data.df = read.table("data/Orland_Park/OP_Provs_by_Friend_Type_and_Loneliness_20
 iRmID = which(is.na(data.df[, "lictot"]))
 data.df = data.df[-iRmID,]
 
+for(c in 4:ncol(data.df)){
+
+	column = colnames(data.df)[c]
+
+	png(file = paste0("output/", column, "_vs_LonelinessScore.png"),
+		width = 7, height = 7, units = "in", res = 300
+	   )
+
+	idx = which(data.df[, "lictot"] > 0)
+
+	# Make the plot
+	plot(x = data.df[idx, column], y = data.df[idx, "lictot"],
+		 xlim = c(0, 10.5), ylim = c(10, 60),
+		 pch = 19, cex = 0.5, col = "#00000050",
+		 xlab = column, ylab = "Loneliness Score"
+		)
+
+	spCor = cor(data.df[idx, column], data.df[idx, "lictot"], method = "spearman") 
+	text(x = 10.5, y = 60, adj = c(1, 1), labels = paste("Spearman cor:", format(spCor, digits = 4)))
+
+	dev.off()
+
+	corCol = "ProvsAllClassmates_8items"
+	cat(column, " vs. lictot Spearman Correlation:\t", spCor,
+		sep = "", end = "\n"
+	   )
+
+}
+
+idx = which(data.df$lictot > 20)
+
+linMod = lm(lictot ~ ProvsRecipFriends_8items + ProvsGivenFriends_8items + ProvsReceivedFriends_8items + ProvsNonFriends_8items, data = data.df)
+linMod = lm(lictot ~ ProvsAllClassmates_8items, data.df)
+linMod = lm(lictot ~ ProvsAllClassmates_8items, data.df)
+
+# glmMod = glm(lictot ~ ProvsAllClassmates_8items, data = data.df, family = gamma)
+
+
+
+plot(x = data.df$lictot, y = as.numeric(linMod$fitted.values),
+	 pch = 19,
+	 xlab = "Loneliness Score (Observed)", ylab = "Loneliness Score (Predicted)"
+	)
+
+if(0){
+
 # Get the total class size
 data.df[, "TotalClassSize"] = rowSums(data.df[, c("CrossSex", "SameSexNoChild")])
+
+
+
 
 # Scale each provision to a class size of 25
 data.df[, "Scaled_ProvsAllClassmates_8items"] = data.df[, "ProvsAllClassmates_8items"] * (data.df[, "TotalClassSize"] / 25)
@@ -27,7 +76,6 @@ for(i in c(4:8, 10)){
 
 cat("\n\n")
 
-if(0){
 
 # Make the plot
 plot(x = data.df[, "Scaled_ProvsAllClassmates_8items"], y = data.df[, "lictot"],
@@ -41,7 +89,6 @@ plot(x = data.df[class_idx, "ProvsAllClassmates_8items"], y = data.df[class_idx,
 	 pch = 19, 
 	 xlab = "Average Provisions All Classmates", ylab = "Loneliness Score"
 	)
-}
 
 # Set the excel file
 f_xlsx = "FriendshipPeerProvisionsByItemAnalysis.xlsx"
@@ -80,5 +127,7 @@ for(itemNum in sheet_names){
 	cat(itemNum, " PeerProvisions Received vs. lictot Spearman Correlation:\t", spCor,
 		sep = "", end = "\n"
        )
+
+}
 
 }

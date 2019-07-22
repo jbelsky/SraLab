7a8db126ab3fbd7ed495c877d4f55ab4fc69c961
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2019-07-22 11:10:27 -0400

    Split full flow into smaller streams

--- Wellesley/create_alluvial_graph.R
+++ Wellesley/create_alluvial_graph.R
@@ -1,12 +1,35 @@
 library("alluvial")
 
+for(j in 1:5){
+
 # Load in the dataset
 data.df = read.table("acquaintance_year1.txt", sep = "\t", header = T)
 
 # Remove "0" rows
 data.df = data.df[which(data.df[,4] > 0),]
 
-png(file = "acquaintance_year1.png", width = 10, height = 7, units = "in", res = 300)
+# Transform into single rows
+data.l = list()
+
+for(i in 1:nrow(data.df)){
+
+	# Get the current row
+	rowVals = data.df[i,]
+	rowVals[4] = 1
+
+	# Get the number of values
+	numVals = data.df[i,4] / j
+	cat(numVals, "\n")
+
+	# Insert into new data frame
+	data.l[[i]] = rbind(rowVals[rep(1, numVals),])
+
+}
+
+# Create the master data.df
+data.df = rbind.fill(data.l)
+
+png(file = paste0("acquaintance_year1_div_by_", j, ".png"), width = 10, height = 7, units = "in", res = 300)
 
 # Create the graph
 alluvial(data.df[,1:3], freq = data.df[,4],
@@ -14,3 +37,5 @@ alluvial(data.df[,1:3], freq = data.df[,4],
 		)
 
 dev.off()
+
+}

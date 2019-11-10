6dd177e597770dd197d745343288b8348846390a
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2019-11-10 18:04:08 -0500

    Created plots and input files for Companionship and Help

--- ProvisionPlots/ProvisionPlot.R
+++ ProvisionPlots/ProvisionPlot.R
@@ -1,58 +1,106 @@
-xx <- c(0.2196,0.4648)   #  <-- change to alter plot dims
-yy <- c(15.9103,20.9087)   #  <-- change to alter plot dims
-x <- c(0.2196,0.4648)   #  <-- x-coords for lines
-y1 <- c(20.9087,16.7433)
-y2 <- c(20.528,17.254)
-y3 <- c(19.9591,18.0174)
+# Set the input parameters
+xleft = -0.3
+xright = 0.3
+ylow = 12
+yhigh = 25
+plot_type = "Help"
 
-slope = c(-16.99, -13.35, -7.92)
-yintercept = c(24.63, 23.45, 21.68)
 
-# Get the corresponding y coordinates for each line
-x1 = 0
-x2 = 0.85
-y1 = numeric(3)
-y2 = numeric(3)
-for (i in 1:3) {
-  y1[i] = slope[i] * x1 + yintercept[i]
-  y2[i] = slope[i] * x2 + yintercept[i]
+param.df = read.table(paste0(plot_type, ".cfg"), sep = "\t", header = T)
+
+# Define the functions
+CalculateSlopeAndYinter = function(ARG_X_PAIR, ARG_Y_PAIR){
+
+	# Get the slope
+	ydiff = ARG_Y_PAIR[1] - ARG_Y_PAIR[2]
+	xdiff = ARG_X_PAIR[1] - ARG_X_PAIR[2]
+
+	m = ydiff / xdiff
+
+	# Get the y-intercept
+	b = ARG_Y_PAIR[1] - m * ARG_X_PAIR[1]
+
+	# Return the slope and y-intercept
+	return(c(m, b))
+
 }
 
+# Get the y-coordinates at the edge of the graphs
+GetYCoords = function(ARG_LINE_PARAMS, ARG_X_PAIR){
 
-png(file = "c:/Users/jab112/Desktop/Mollaulasher.png",
-    width = 7,
-    heigh = 7,
-    units = "in",
-    res = 300
-    )
-par(mar = c(5, 5, 2, 2),family = "serif")
-plot(0,0,type='n',
-     xlab = "",
-     ylab = "",
-     main = "",
-     xlim = c(0, 0.85),
-     xaxt = "n",
-     ylim = c(10.1885, 24.63)
-    )
-axis(1, at = seq(0.1, 0.7, 0.2), labels = F, tcl = par()$tcl * 0.7)
-axis(1, at = seq(0, 0.8, 0.2), labels = c("0", "0.2", "0.4", "0.6", "0.8"))
-axis(2, at = seq(12.5, 22.5, 5), labels = F, tcl = par()$tcl * 0.7)
-title(ylab = "Loneliness", xlab = "Child-Level Provisions Received (SG)", cex.lab = 1)
+	# Unpack the line parameters
+	m = ARG_LINE_PARAMS[1]
+	b = ARG_LINE_PARAMS[2]
 
-for (i in 1:3){
-  lines(x = c(x1, x2), y = c(y1[i], y2[i]), lwd = 2, lty = i)
+	# Get the y-value at each x-value
+	y_pair = m * ARG_X_PAIR + b
+
+	return(y_pair)
+
 }
 
-#lines(x,y1,lwd=3,lty=1)
-#lines(x,y2,lwd=3,lty=2)
-#lines(x,y3,lwd=3,lty=3)
-legend("bottomleft",
-       legend = c(expression(paste("Low-Provisions Class (20"^"th", " Percentile)")),
-                  expression(paste("Average-Provisions Class (50"^"th", " Percentile)")),
-                  expression(paste("High-Provisions Class (80"^"th", " Percentile)"))
-                  ),
-       lwd = 3, lty = c(1, 2, 3),
-       bty = "n"
-      )
+# Make the plot
+MakePlot = function(ARG_CHART, ARG_X_PAIR, ARG_Y_PAIR, ARG_Y_LIST, ARG_SAVE_PNG = F){
 
-dev.off()
+	if(ARG_SAVE_PNG){
+		png(file = paste0(ARG_CHART, ".png"), width = 7, height = 7, units = "in", res = 300)
+	}
+
+	# Initialize the plot
+	par(mar = c(5, 5, 2, 2),family = "serif")
+	plot(0, 0, type='n',
+		 xlab = "", ylab = "", main = "",
+		 xlim = ARG_X_PAIR, xaxt = "n",
+		 ylim = ARG_Y_PAIR
+		)
+
+	# Set the axes
+	axis(1, at = seq(-0.95, 0.95, 0.1), labels = F, tcl = par()$tcl * 0.7)
+	axis(1, at = seq(-1, 1, 0.1))
+	axis(2, at = seq(11, 25, 2), labels = F, tcl = par()$tcl * 0.7)
+	axis(2, at = seq(10, 26, 2))
+	title(ylab = "Loneliness", xlab = paste0("Child-Level ", ARG_CHART, " Provisions Received (SG)"), cex.lab = 1)
+
+	# Draw the lines
+	for(i in 1:3){
+		lines(ARG_X_PAIR, ARG_Y_LIST[[i]], lwd=3, lty=i)
+	}
+
+	legend("bottomleft",
+		   legend = sapply(
+							c(bquote(paste("Low-", .(ARG_CHART), " Class (", 20^th, " Percentile)")),
+		   			  		  bquote(paste("Average-", .(ARG_CHART), " Class (", 50^th, " Percentile)")),
+		   			          bquote(paste("High-", .(ARG_CHART), " Class (", 80^th, " Percentile)"))
+					         ), 
+                            as.expression
+                           ),
+		   lwd = 3, lty = c(1, 2, 3),
+		   bty = "n"
+		  )
+
+	if(ARG_SAVE_PNG){
+		dev.off()
+	}
+
+}
+
+# Initialize the ycoord list
+y.l = vector(mode = "list", 3)
+
+# Get the x-coordinates
+x = as.numeric(param.df[which(param.df$pts == "x"), 2:3])
+xpair = c(xleft, xright)
+for(i in 1:3){
+
+	# Get the y coordinates
+	ycoords = as.numeric(param.df[(i+1), 2:3])
+
+	# Get the line parameters
+	lparam = CalculateSlopeAndYinter(x, ycoords)
+
+	# Get the y-coordinates for the pair
+	y.l[[i]] = GetYCoords(lparam, xpair)
+
+}
+
+MakePlot(plot_type, xpair, c(ylow, yhigh), y.l, T)

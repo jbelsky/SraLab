ad256d6c7430ffc7cea5424d797515ee8dd1736c
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2021-02-19 11:14:06 -0500

    Finalized extraction of which friend(s) were the min/max value of a metric among the 3 friends

--- multiple_max_positive_features_20210124.py
+++ multiple_max_positive_features_20210124.py
@@ -4,29 +4,59 @@ import numpy as np
 import re
 import sys
 
-def get_friend_index(scores, max_score):
+def get_friend_index(row, metric_name):
+
+    # Get the scores for each friend
+    friends_scores = get_friend_metrics(row, metric_name)
 
     # Create an np.array of the scores
-    scores = np.array(scores)
-    num_nan_scores = len(np.isnan(scores).nonzero()[0])
+    scores = np.array(friends_scores)
 
-    if num_nan_scores > 1 and np.isnan(max_score):
+    # Get the friend(s) with the minimum score
+    friend_min = get_friend_subset(scores, row["Min" + metric_name])
+    friend_max = get_friend_subset(scores, row["Max" + metric_name])
+
+    return friend_min, friend_max
+
+
+def get_friend_subset(friend_scores, value):
+
+    num_nan_scores = len(np.isnan(friend_scores).nonzero()[0])
+
+    if num_nan_scores > 1 and np.isnan(value):
         return np.NaN
-    elif all(np.isnan(scores)):
-        print(f"Max score is {max_score} but scores are not all np.NaN!")
+    elif all(np.isnan(friend_scores)):
+        print(f"Min/Max score is {value} but scores are not all np.NaN!")
         return -1
-    elif np.isnan(max_score) and num_nan_scores < 2:
-        print(f"Max score is np.NaN but not all scores are np.NaN!")
+    elif np.isnan(value) and num_nan_scores < 2:
+        print(f"Min/Max score is np.NaN but not all scores are np.NaN!")
         return -2
 
-    # Otherwise, find the index of the max score
-    max_score_idx = np.nonzero(scores == max_score)[0]
-    max_score_idx += 1
+    # Otherwise, find the index of the minmax score
+    score_idx = np.nonzero(friend_scores == value)[0]
+    score_idx += 1
 
-    if len(max_score_idx) > 1:
-        return "".join([str(x) for x in max_score_idx])
+    if len(score_idx) > 1:
+        return "".join([str(x) for x in score_idx])
     else:
-        return max_score_idx[0]
+        return score_idx[0]
+
+
+
+
+def get_friend_metrics(row, metric_name):
+
+    # Create the list
+    metric_list = []
+
+    # Append each friend score to the list
+    for friend in [1, 2, 3]:
+        colname = f"F{friend}FFQ_{metric_name}Avg"
+        value = row[colname]
+        metric_list.append(value)
+
+    return metric_list
+
 
 
 # Read in the csv file
@@ -39,7 +69,14 @@ metrics = []
 for colname in scores_df.columns[0:11]:
     m = p.match(colname)
     try:
-        metrics.append(m.group(1))
+
+        # Get the metric
+        metric = m.group(1)
+
+        # Don't have M[in|ax][SA|C]
+        if metric not in ["SA", "C"]:
+            metrics.append(metric)
+
     except AttributeError:
         print(f"Could not extract metric from column name {colname}, exiting...")
         sys.exit(1)
@@ -47,6 +84,7 @@ for colname in scores_df.columns[0:11]:
 # Create the column names for the output_df
 colnames_out = []
 for metric in metrics:
+
     # Suffix metric with "Friends"
     metric_with_suffix = metric + "Friends"
 
@@ -57,23 +95,17 @@ for metric in metrics:
 # Make the output df
 out_df = pd.DataFrame(index=scores_df.index, columns=colnames_out)
 
-'''
 # Iterate through each row
-for id, vals in max_scores_df.iterrows():
-
-    col_idx = 0
+for id, vals in scores_df.iterrows():
 
     # Iterate through each of the columns
-    for col in out_df.columns:
+    for metric in metrics:
 
         # Get the return val
-        val = get_friend_index(vals.iloc[col_idx:(col_idx + 3)], vals[col])
+        fr_min, fr_max = get_friend_index(vals, metric)
 
         # Insert into the out_df
-        out_df.loc[id, col] = val
+        out_df.loc[id, "Min" + metric + "Friends"] = fr_min
+        out_df.loc[id, "Max" + metric + "Friends"] = fr_max
 
-        # Iterate the column index
-        col_idx += 3
-
-out_df.to_csv("max_scores.txt", sep="\t", na_rep=-9)
-'''
+out_df.to_csv("multiple_max_positive_features.txt", sep="\t", na_rep=-9)

0fad2582f5e55fc0f3e319321da501382c715c2d
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2021-02-19 10:43:40 -0500

    Read in data and create output df

--- multiple_max_positive_features_20210124.py
+++ multiple_max_positive_features_20210124.py
@@ -1,6 +1,8 @@
+import itertools
 import pandas as pd
 import numpy as np
-
+import re
+import sys
 
 def get_friend_index(scores, max_score):
 
@@ -27,11 +29,35 @@ def get_friend_index(scores, max_score):
         return max_score_idx[0]
 
 
-max_scores_df = pd.read_excel("Max Scores 1.23.21.xlsx", index_col=0, na_values=-9)
+# Read in the csv file
+scores_df = pd.read_csv("Multiple Max Positive Features 1.24.21.csv", index_col=0, na_values=[-9, "", " "])
+
+# Extract out the metric from the column name
+# 11 metrics of form F[123]FFQ_([A-Za-z]{1,3})Avg
+p = re.compile("F[123]FFQ_([A-Za-z]{1,3})Avg")
+metrics = []
+for colname in scores_df.columns[0:11]:
+    m = p.match(colname)
+    try:
+        metrics.append(m.group(1))
+    except AttributeError:
+        print(f"Could not extract metric from column name {colname}, exiting...")
+        sys.exit(1)
+
+# Create the column names for the output_df
+colnames_out = []
+for metric in metrics:
+    # Suffix metric with "Friends"
+    metric_with_suffix = metric + "Friends"
+
+    # Prefix "Min" and "Max" to colnames
+    colnames_out.append("Min" + metric_with_suffix)
+    colnames_out.append("Max" + metric_with_suffix)
 
 # Make the output df
-out_df = pd.DataFrame(index=max_scores_df.index, columns=max_scores_df.columns[-4:-1])
+out_df = pd.DataFrame(index=scores_df.index, columns=colnames_out)
 
+'''
 # Iterate through each row
 for id, vals in max_scores_df.iterrows():
 
@@ -50,3 +76,4 @@ for id, vals in max_scores_df.iterrows():
         col_idx += 3
 
 out_df.to_csv("max_scores.txt", sep="\t", na_rep=-9)
+'''
